59cd9a7b8b8db5a18bdf69981c0af0d005a961f1
Aleksander Dembskiy <adembskiy@fiksu.com>
2013-11-10 00:59:01 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jonny::Application.config.secret_token = 'bacf7c80c390a09488a7f6623239ef5949f3654e0d4e31eb2eed9258f8a6987b10d80a9645fa46cef04a5def09eb8e79537760ede14d5467f20d07c239863859'
