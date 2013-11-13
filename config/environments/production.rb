60494116897a638161de4e9ce4d901b7dcd7c817
Aleksander Dembskiy <adembskiy@fiksu.com>
2013-11-13 04:45:56 +0400

    fixed format of manifest file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Jonny::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.sass, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( administration.sass )
+  config.assets.precompile += %w(administration.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
